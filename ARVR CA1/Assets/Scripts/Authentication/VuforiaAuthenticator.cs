f5607a1070ba866c36dec2abf0526e848d67c8c0
CharredBaguette <47597056+CharredBaguette@users.noreply.github.com>
2020-08-04 15:47:17 +0800

    Create VuforiaAuthenticator.cs
    A script to read the license key from a file. Sets the value to
    VuforiaConfiguration license key before runtime initializiation.
    This process excludes the license key from being pushed to the online
    repository.

---
+++ ARVR CA1/Assets/Scripts/Authentication/VuforiaAuthenticator.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// This script initializes the Vuforia license key during runtime.
+/// This script is created to exclude the license key from version controls systems.
+/// Specify the relative file path to the license key and remember to exclude it from version control
+/// </summary>
+namespace Authentication
+{
+    public class LicenseKey : ScriptableObject
+    {
+        /// The license key file path relative to Application.dataPath
+        private const string LicenseKeyRelativePath = "/Secrets/vuforia";
+
+        // Set the Vuforia License Key when the application is launched
+        [RuntimeInitializeOnLoadMethod]
+        private static void SetLicenseKey()
+        {
+            var config = VuforiaConfiguration.Instance.Vuforia;
+            config.DelayedInitialization = true;
+            config.LicenseKey = ReceiveLicenseKey();
+        }
+
+        private static string ReceiveLicenseKey()
+        { 
+            string licenseFilePath = string.Concat(Application.dataPath, LicenseKeyRelativePath);
+
+            return File.ReadAllText(licenseFilePath);
+        }
+    }
+}
