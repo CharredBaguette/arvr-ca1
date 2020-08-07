ea5385fde4dfce1c9b87a3183720f65913838d45
CharredBaguette <47597056+CharredBaguette@users.noreply.github.com>
2020-08-07 12:04:40 +0800

    Add CameraAutoFocusBehaviour.cs
    Create MonoBehaviour for camera autofocus behaviour

---
+++ ARVR CA1/Assets/Scripts/Camera/CameraAutoFocusBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Vuforia;
+
+/// <summary>
+/// Class with camera focus functions for Vuforia.
+/// Script partly referenced from : https://developer.vuforia.com/forum/unity-extension-technical-discussion/blurry-camera-image
+/// </summary>
+namespace Camera
+{
+    public class CameraAutoFocusBehaviour : MonoBehaviour
+    {
+        private CameraDevice _cameraDevice;
+
+        private void Start()
+        {
+            var vuforia = VuforiaARController.Instance;
+            vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+            vuforia.RegisterOnPauseCallback(OnPaused);
+        }
+
+        private void OnVuforiaStarted()
+        {
+            CameraDevice.Instance.SetFocusMode(
+                CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+
+        private void OnPaused(bool paused)
+        {
+            if (!paused) // resumed
+            {
+                // Set again autofocus mode when app is resumed
+                CameraDevice.Instance.SetFocusMode(
+                    CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+            }
+        }
+    }
+}
