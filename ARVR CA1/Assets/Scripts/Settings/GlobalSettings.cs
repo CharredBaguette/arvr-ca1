6963a4905ef3783b1ccd8e79328a15068aa59717
CharredBaguette <47597056+CharredBaguette@users.noreply.github.com>
2020-08-08 17:25:31 +0800

    Clamp volume  variable in GlobalSettings.cs
    Add argument validation in SetMasterVolume() to clamp volume argument
    between 0 and 1.0f.

--- ARVR CA1/Assets/Scripts/Settings/GlobalSettings.cs
+++ ARVR CA1/Assets/Scripts/Settings/GlobalSettings.cs
@@ -1,12 +1,10 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;
-
-[CreateAssetMenu(fileName="Global Settings", menuName="GlobalSettings")]
+[CreateAssetMenu(fileName = "Global Settings", menuName = "GlobalSettings")]
 public class GlobalSettings : ScriptableObject
 {
     [SerializeField] private AudioMixer _audioMixer;
     [SerializeField] private string _volumeParameter = "Master";
-
     private float _currentVolume = 1.0f;
 
     private void OnEnable()
@@ -17,13 +15,20 @@ public class GlobalSettings : ScriptableObject
 
     private void OnDisable()
     {
+        // Save volume to playerprefs
         PlayerPrefs.SetFloat(_volumeParameter, _currentVolume);
     }
 
+    /// <summary>
+    /// Set master audio mixer's volume parameter
+    /// </summary>
+    /// <param name="volume"></param>
     public void SetMasterVolume(float volume)
     {
+        // Clamp the volume to 0 and 1.0f
+        volume = Mathf.Clamp01(volume);
+
         _currentVolume = volume;
-        
         // Set audio mixer master volume
         _audioMixer.SetFloat(_volumeParameter, Mathf.Log10(volume) * 20);
     }
