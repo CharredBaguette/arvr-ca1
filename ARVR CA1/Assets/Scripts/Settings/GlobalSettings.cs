22cab30db93ce40969637fbf9a0309bf917f7fda
CharredBaguette <47597056+CharredBaguette@users.noreply.github.com>
2020-08-08 16:03:28 +0800

    Add GlobalSettings.cs
    Create script to provide persistent settings data.

---
+++ ARVR CA1/Assets/Scripts/Settings/GlobalSettings.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[CreateAssetMenu(fileName="Global Settings", menuName="GlobalSettings")]
+public class GlobalSettings : ScriptableObject
+{
+    [SerializeField] private AudioMixer _audioMixer;
+    [SerializeField] private string _volumeParameter = "Master";
+
+    private float _currentVolume = 1.0f;
+
+    private void OnEnable()
+    {
+        // Assign from player prefs
+        _currentVolume = PlayerPrefs.GetFloat(_volumeParameter, 1.0f);
+    }
+
+    private void OnDisable()
+    {
+        PlayerPrefs.SetFloat(_volumeParameter, _currentVolume);
+    }
+
+    public void SetMasterVolume(float volume)
+    {
+        _currentVolume = volume;
+        
+        // Set audio mixer master volume
+        _audioMixer.SetFloat(_volumeParameter, Mathf.Log10(volume) * 20);
+    }
+}
