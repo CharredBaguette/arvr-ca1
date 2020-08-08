da97751d819c1cf8c7956f75d5909d18c17f9001
CharredBaguette <47597056+CharredBaguette@users.noreply.github.com>
2020-08-09 02:28:22 +0800

    Add Ingredient.cs
    Create ScriptableObject to hold ingredient data.

---
+++ ARVR CA1/Assets/Scripts/Recipes/Ingredient.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Recipes
+{
+    /// <summary>
+    /// An ingredient used in recipes.
+    /// </summary>
+    [CreateAssetMenu(fileName = "NewIngredient", menuName = "Recipes/Ingredient")]
+    public class Ingredient : ScriptableObject
+    {
+        [SerializeField] private string _pluralName;
+        /// <summary>
+        /// Name in plural form
+        /// </summary>
+        public string PluralName => _pluralName;
+
+        [SerializeField] private GameObject _model;
+        /// <summary>
+        /// Model of this ingredient
+        /// </summary>
+        public GameObject Model => _model;
+    }
+}
