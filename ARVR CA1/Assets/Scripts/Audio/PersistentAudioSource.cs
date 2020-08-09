f0c227be497f03847c25958cda451e08b03f7f14
CharredBaguette <47597056+CharredBaguette@users.noreply.github.com>
2020-08-09 21:50:09 +0800

    Add PersistentAudioSource.cs

---
+++ ARVR CA1/Assets/Scripts/Audio/PersistentAudioSource.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class PersistentAudioSource : MonoBehaviour
+{
+    private static PersistentAudioSource _instance;
+
+    private void Awake()
+    {
+        if (!_instance)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
